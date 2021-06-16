20f7a706d6f9c4cac9ebbf3eeb63b90d88441f63
Alma-field <66554636+Alma-field@users.noreply.github.com>
2021-06-16 16:27:27 +0900

    Add module data

---
+++ twitcaspy/__init__.py
@@ -0,0 +1,11 @@
+# Twitcaspy
+# Copyright 2021 Alma-field
+# See LICENSE for details.
+
+"""
+Twitcaspy : Twitcasting API library
+"""
+__version__ = '0.0.0'
+__author__ = 'Alma-field'
+__license__ = 'MIT'
+
