fc0489cc21ecf7c63e10470fb86b08816c7a223e
Alma-field <66554636+Alma-field@users.noreply.github.com>
2021-06-16 22:29:41 +0900

    Add model class

---
+++ twitcaspy/models/__init__.py
@@ -0,0 +1,6 @@
+# Twitcaspy
+# Copyright 2021 Alma-field
+# See LICENSE for details.
+
+from .model import Model
+
---
+++ twitcaspy/models/model.py
@@ -0,0 +1,25 @@
+# Twitcaspy
+# Copyright 2021 Alma-field
+# See LICENSE for details.
+
+class Model:
+    def __init__(self, api=None):
+        self._api = api
+
+    def __getstate__(self):
+        # pickle
+        pickle = dict(self.__dict__)
+        try:
+            del pickle['_api']  # do not pickle the API reference
+        except KeyError:
+            pass
+        return pickle
+
+    @classmethod
+    def parse(cls, api, json):
+        """Parse a JSON Model into a model instance."""
+        raise NotImplementedError
+
+    def __repr__(self):
+        state = [f'{k}={v!r}' for (k, v) in vars(self).items()]
+        return f'{self.__class__.__name__}({", ".join(state)})'
