14ce7b99ebf073baeac631c223065cbfc1b7539d
Alma-field <66554636+Alma-field@users.noreply.github.com>
2021-06-20 18:01:40 +0900

    Update: Raise TypeError when client id and/or secret is not of type string
    Co-Authored-By: null <Alma-field@users.noreply.github.com>

--- twitcaspy/auth/oauth/basic.py
+++ twitcaspy/auth/oauth/basic.py
@@ -22,6 +22,13 @@ class OAuth2Basic(AuthBase):
         If the given client id and/or secret is not a string instance
     """
     def __init__(self, client_id, client_secret):
+        if not isinstance(client_id, str):
+            raise TypeError("ClientID must be string, not "
+                            + type(client_id).__name__)
+        if not isinstance(client_secret, str):
+            raise TypeError("Client secret must be string, not "
+                            + type(client_secret).__name__)
+
         self.token = b64encode(f'{client_id}:{client_secret}'.encode('utf-8'))
         self.token = self.token.decode('utf-8')
 
