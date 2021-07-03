627d4cd6507df98f46d30aa1e0b89f5dc90fc41e
Alma-field <66554636+Alma-field@users.noreply.github.com>
2021-07-03 13:37:02 +0900

    Update: Specify utf-8 as the encoding when opening the file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if match:
 else:
     raise RuntimeError(f"Unable to find version string in {VERSION_FILE}.")
 
-with open("README.md") as readme_file:
+with open("README.md", "r", encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 tests_require = [
